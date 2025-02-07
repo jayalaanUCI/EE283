17b8aada9bc4323874ab2c45543cf2ecd6f532bc
jayalaanUCI <jayalaan@uci.edu>
2025-02-06 21:44:34 -0800

    quick fix

--- HW3/prob4/DNAseqAlign.sh
+++ HW3/prob4/DNAseqAlign.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=DNAseqAlign
+#SBATCH --job-name=ATAcseqAlign
 #SBATCH -A class-ecoevo283
 #SBATCH -p standard
 #SBATCH --cpus-per-task=8
-#SBATCH --error=DNAseqAlign.%A_%a.err
-#SBATCH --output=DNAseqAlign.%A_%a.out
+#SBATCH --error=ATACseqAlign.%A_%a.err
+#SBATCH --output=ATACseqAlign.%A_%a.out
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=1-00:00:00
-#SBATCH --output=/pub/jayalaan/EE283/HW3/prob4/DNAseq/DNAout
 #SBATCH --array=1-12
 
 module load bwa/0.7.8
 module load samtools/1.15.1
 
 files="/pub/jayalaan/EE283/HW3/prob4/prefix.txt"
-prefix='head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1'
+prefix=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1`
 
 data="/pub/jayalaan/EE283/HW2/data/DNAseq"
 out="/pub/jayalaan/EE283/HW3/prob4/DNAout"
@@ -23,7 +22,5 @@ THREADS=8
 
 echo $prefix
 
-bwa mem -t $THREADS $ref ${data}/${prefix}_1.fq.gz ${data}/${prefix}_2.fq.gz > ${prefix}.sam
-samtools sort -@ $THREADS -m 4G -o ${out}/${prefix}.sort.bam ${prefix}.sam && rm ${prefix}.sam
-
+bwa mem -t $THREADS $ref ${data}/${prefix}_1.fq.gz ${data}/${prefix}_2.fq.gz | samtools sort - -@ $THREADS -m 4G -o ${out}/${prefix}.sort.bam
 
