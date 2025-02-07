ce30b21de1b63879962ae92603c091e84ca3d4db
jayalaanUCI <jayalaan@uci.edu>
2025-02-06 21:41:45 -0800

    fixed ATACseqAlign and DNAseqAlign code

--- HW3/prob4/DNAseqAlign.sh
+++ HW3/prob4/DNAseqAlign.sh
@@ -2,25 +2,28 @@
 #SBATCH --job-name=DNAseqAlign
 #SBATCH -A class-ecoevo283
 #SBATCH -p standard
-#SBATCH --cpus-per-task=12
-#SBATCH --error=DNAseAlign.%A_%a.err
-#SBATCH --output=RealDNAseqAlign.%A_%a.out
-#SBATCH --mem-per-cpu=3G
+#SBATCH --cpus-per-task=8
+#SBATCH --error=DNAseqAlign.%A_%a.err
+#SBATCH --output=DNAseqAlign.%A_%a.out
+#SBATCH --mem-per-cpu=4G
 #SBATCH --time=1-00:00:00
-#SBATCH --output=/pub/jayalaan/EE283/HW3/prob4/DNAseq/
+#SBATCH --output=/pub/jayalaan/EE283/HW3/prob4/DNAseq/DNAout
 #SBATCH --array=1-12
 
 module load bwa/0.7.8
 module load samtools/1.15.1
 
 files="/pub/jayalaan/EE283/HW3/prob4/prefix.txt"
-prefix='head -n $SLURM_ARRAY_TASK_ID $files | tail -1 n'
+prefix='head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1'
+
 data="/pub/jayalaan/EE283/HW2/data/DNAseq"
 out="/pub/jayalaan/EE283/HW3/prob4/DNAout"
-index="/pub/jayalaan/EE283/HW3/ref/index/bwaindex"
 ref="/pub/jayalaan/EE283/HW3/ref/dmel-all-chromosome-r6.13.fasta"
+THREADS=8
 
-bwa mem  -t $SLURM_CPUS_PER_TASK -M ${ref} ${data}/${prefix}_1.fq.gz ${data}/${prefix}_2.fq.gz | samtools view -bS - > ${out}/${prefix}.sort.bam
-samtools sort - -@ $SLURM_CPUS_PER_TASK -m 4G -o ${out}/${prefix}.bam -o ${output}/${prefix}.sort.bam
-samtools index ${out}/${prefix}.sort.bam
+echo $prefix
+
+bwa mem -t $THREADS $ref ${data}/${prefix}_1.fq.gz ${data}/${prefix}_2.fq.gz > ${prefix}.sam
+samtools sort -@ $THREADS -m 4G -o ${out}/${prefix}.sort.bam ${prefix}.sam && rm ${prefix}.sam
+
 
